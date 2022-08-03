b80013cae63d11f4efd9362bbf44ae7eefacae22
WonSik36 <cws0173@gmail.com>
2022-08-04 06:41:22 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/demo/stream/DemoStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoStreamApplication.class, args);
+    }
+}
